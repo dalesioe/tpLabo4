ff9d19bda8b88cd3fdd266fae15f2edd92bd10cd
dalesioe <dalesioe@gmail.com>
2018-05-01 19:55:46 -0300

    front login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { JuegosComponent } from './componentes/juegos/juegos.component';
@@ -22,7 +22,8 @@ import { RuteoModule } from './ruteo/ruteo.module';
   ],
   imports: [
     BrowserModule,
-    RuteoModule
+    RuteoModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
