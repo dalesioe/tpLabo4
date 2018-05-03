b07df9634f8dabc0026f3bd1d96eb250c12f7029
dalesioe <dalesioe@gmail.com>
2018-05-03 11:46:02 -0300

    Empiezo grilla de juegos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AdivinaComponent } from './componentes/adivina/adivina.component';
 import { PptComponent } from './componentes/ppt/ppt.component';
 import { AhorcadoComponent } from './componentes/ahorcado/ahorcado.component';
 import { RuteoModule } from './ruteo/ruteo.module';
-
+import { DataGridModule } from 'primeng/datagrid';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,8 @@ import { RuteoModule } from './ruteo/ruteo.module';
   imports: [
     BrowserModule,
     RuteoModule,
-    FormsModule
+    FormsModule,
+    DataGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/juegos/juegos.component.ts
+++ src/app/componentes/juegos/juegos.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./juegos.component.css']
 })
 export class JuegosComponent implements OnInit {
-
-  constructor() { }
+  juegos: Object[];
+  constructor() {
+    this.juegos = [
+      { nombre: 'Adivina el numero', imagen: 'adivina', link: '/adivina', color: 'Black' },
+      { nombre: 'Piedra Papel Tijera', imagen: 'ppt', link: '/ppt', color: 'White' },
+      { nombre: 'Juego 3', imagen: 'juego3', link: '/juego3', color: 'Blue' },
+      { nombre: 'Puntaje', imagen: 'puntaje', link: '/puntaje', color: 'Blue' },
+      { nombre: 'Acerca de', imagen: 'acercade', link: '/acercade', color: 'Blue' }
+    ];
+   }
 
   ngOnInit() {
   }
